5084ee1a904df87a844511ce4c7822eb9c512fad
iGufrankhan <kaka.babawasim12345@gmail.com>
2025-04-17 07:17:08 +0530

    project

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pickle  # or import pickle if you used that
+
+app = Flask(__name__)
+
+with open("classifier.pkl", "rb") as file:
+    classifier = pickle.load(file)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        features = [float(request.form[key]) for key in request.form]
+        data = np.array([features])
+        prediction = classifier.predict(data)
+        result = "Diabetic" if prediction[0] == 1 else "Not Diabetic"
+        return render_template('result.html', result=result)
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
